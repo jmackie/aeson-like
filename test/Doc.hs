d0647316d649a72a70f25bfae1e2166c9d354bd1
Jordan Mackie <jordan.mackie@habito.com>
2019-06-24 14:04:13 +0100

    Add doctests

---
+++ test/Doc.hs
@@ -0,0 +1,10 @@
+module Main
+  ( main
+  ) where
+
+import           Prelude
+
+import qualified Test.DocTest as DocTest
+
+main :: IO ()
+main = DocTest.doctest ["src"]
--- test/Main.hs
+++ test/Main.hs
@@ -10,10 +10,9 @@ import qualified Data.Aeson.EnumLike.Tests   as EnumLike
 import qualified Data.Aeson.ObjectLike.Tests as ObjectLike
 
 main :: IO ()
-main = Tasty.defaultMain tests
-
-tests :: Tasty.TestTree
-tests = Tasty.testGroup "Tests"
-  [ ObjectLike.tests
-  , EnumLike.tests
-  ]
+main = 
+  Tasty.defaultMain $
+    Tasty.testGroup "Tests"
+      [ ObjectLike.tests
+      , EnumLike.tests
+      ]
